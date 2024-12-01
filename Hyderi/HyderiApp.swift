9f2bebdffb9c670dac1b69306fbcf68c9a66b629
Ball Don't Lie <razimehdiali@gmail.com>
2024-12-01 22:32:20 +0000

    Initial Commit

---
+++ Hyderi/HyderiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HyderiApp.swift
+//  Hyderi
+//
+//  Created by Ali Earp on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HyderiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
